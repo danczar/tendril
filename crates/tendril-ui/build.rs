3f0f797890098d2861a2db41cce138ec86d48087
Dan Czarnecki <danczar@gmail.com>
2026-02-23 22:26:35 -0500

    added windows ico and updated windows build config

--- crates/tendril-ui/build.rs
+++ crates/tendril-ui/build.rs
@@ -1,3 +1,10 @@
 fn main() {
     slint_build::compile("ui/main-window.slint").unwrap();
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("ui/tendril.ico");
+        res.compile().unwrap();
+    }
 }
